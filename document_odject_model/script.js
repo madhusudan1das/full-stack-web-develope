5000392361d3a1b65c3f9419788e81f1a2287a3d
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-03-25 21:41:07 +0530

    html css java script

--- document_odject_model/script.js
+++ document_odject_model/script.js
@@ -3,3 +3,7 @@ document.getElementById("click").addEventListener("click", function() {
     title.textContent = "Hello Madhusudan"; // Change text
     title.style.color = "blue"; // Change color
 });
+
+console.dir(document.head.childNodes[1]);
+document.body.style.background="red";
+document.getElementById("title").innerHTML="hello"
