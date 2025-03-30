8f318d1a43c9c608a2d98837b59d5db3eddfa1e0
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-03-30 09:46:19 +0530

    java script

---
+++ document_odject_model/style change/script.js
@@ -0,0 +1,4 @@
+const element=document.getElementById("div");
+element.style.color="red";
+element.style.backgroundColor="blue" //this is style in js forchange the color of html elements
+element.style.fontSize="15px"
