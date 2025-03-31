0cd5b947d479c04bf59ae822d217a07722ab36e8
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-03-31 10:35:55 +0530

    project

---
+++ projects/style_change/script.js
@@ -0,0 +1,5 @@
+const element=document.getElementById("div");
+element.style.color="red";
+element.style.backgroundColor="blue" //this is style in js forchange the color of html elements
+element.style.fontSize="15px"
+element.style.visibility="hidden"
