e145ad4968b2d0ba0834812c1960e8e2e15d89c9
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-04-10 23:36:53 +0530

    java script

---
+++ java script c-w-h/object_change_value.js
@@ -0,0 +1,14 @@
+const student={
+    name:"sanu",
+    class:12,
+    year:"3rd_year",
+    marks:8.0,
+
+};
+{
+student['name']="ami";
+student['friends']="none";              //update 
+student["class"]=7;
+}
+console.log(student.name);
+console.log(student);
---
+++ java script c-w-h/print_type.js
@@ -0,0 +1,5 @@
+let a="89";
+let b=70;
+let sum=a+b;
+console.log(sum);
+console.log(typeof(sum));
