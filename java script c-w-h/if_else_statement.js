9a44ca7fc5a6f3b75d7f91158d55e49a3c5d2a46
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-04-13 13:42:43 +0530

    java script

---
+++ java script c-w-h/if_else_statement.js
@@ -0,0 +1,12 @@
+let age=prompt("enter your age: ");
+if(age<18){
+    alert("kid");
+}else if(age<18){
+    alert("can`t dirve")
+        
+    }else if(age>18){
+        alert("you are adault")
+    }
+else{
+    alert("you can drive")
+};
