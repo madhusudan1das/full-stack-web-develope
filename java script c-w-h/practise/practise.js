9f5ff990bc6c092887e9075c8fa8ff2f94841843
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-04-14 11:46:01 +0530

    java script

---
+++ java script c-w-h/practise/practise.js
@@ -0,0 +1,2 @@
+let a=9;
+console.log(a>=10&&a<=20? "lies":"not");
---
+++ java script c-w-h/practise/practise2.js
@@ -0,0 +1,10 @@
+let age=12;
+let r;
+switch(true){
+  case (age>18):
+    r=("adault");
+    break;
+  default:
+    r=("not adault");
+}
+console.log(`${r}`);
