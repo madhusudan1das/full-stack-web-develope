07537d05bbc4fda6613085e612f8b372b591dcf0
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-04-15 22:25:59 +0530

    java script

---
+++ java script c-w-h/while_loop.js
@@ -0,0 +1,6 @@
+let i=1;
+let n=8;
+while(i<=n){
+    console.log(i,"i love java script");
+    i++;
+}
--- java script front-end/do_while_loop.js
+++ java script front-end/do_while_loop.js
@@ -1,5 +1,11 @@
+// let i=0;
+// do{
+//     console.log("hello madhusudan");
+//     i++;
+// }while(i<=5);
+
 let i=0;
-do{
-    console.log("hello madhusudan");
+while(i<10){
+    console.log(i,"sanu");
     i++;
-}while(i<=5);
+};
