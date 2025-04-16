bc29c2756ebf99993d5791d875f72a62795c8db7
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-04-16 11:13:45 +0530

    java script

---
+++ java script c-w-h/function_addition.js
@@ -0,0 +1,22 @@
+// function add(a,b){
+//     console.log("😍")
+//     return a+b;
+// }
+// let a=5;
+// let b=3;
+// console.log(add(a,b));
+
+
+
+
+// const sum =(a,b)=>{
+//     return a+b;
+// }
+// console.log(sum(4,5));
+
+
+
+const hello=()=>{
+    console.log("hello madhusudan");
+}
+let a=hello();
