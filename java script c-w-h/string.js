9ee880802e43b5414486277640136da0096c60d6
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-04-18 08:20:54 +0530

    java script

--- java script c-w-h/string.js
+++ java script c-w-h/string.js
@@ -1,4 +1,6 @@
 let str=("sanu");
+let str1=("monu");
 console.log(str);
 console.log(str.length);
 console.log(str[2]);
+console.log(`${str}  and  ${str1} are lover no one can\`t leave `);
---
+++ java script c-w-h/tring method.js
@@ -0,0 +1,5 @@
+let st="madhu"
+console.log(st.length)
+console.log("he is the \n  biggest criminal \ t in the \r wolrd   \e cjnc")
+
+console.log(st.toUpperCase());
