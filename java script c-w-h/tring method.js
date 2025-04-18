b981cb80029b3ce9cda1aa98682cc2a3165ce844
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-04-18 09:40:13 +0530

    java script

--- java script c-w-h/tring method.js
+++ java script c-w-h/tring method.js
@@ -1,5 +1,9 @@
 let st="madhu"
 console.log(st.length)
-console.log("he is the \n  biggest criminal \ t in the \r wolrd   \e cjnc")
+// console.log("he is the \n  biggest criminal \ t in the \r wolrd   \e cjnc") 
 
-console.log(st.toUpperCase());
+console.log(st.toUpperCase()); //convert uppercase
+console.log(st.slice(2, 3))  //remove value of 2 or 3
+console.log(st.replace("ma","sa"));  //replace the value of ma
+let friend="riya"
+console.log(st.concat(" love lot of ", friend))
