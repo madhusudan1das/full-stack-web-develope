91d9eb7c5de8bac87bd0a1bafad841249c50866e
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-07-29 21:37:51 +0530

    java script

---
+++ java script c-w-h/variable/variable.js
@@ -0,0 +1,4 @@
+let a=3;
+const b=4;
+var c=5;
+console.log(a);
---
+++ java script c-w-h/variables.js/let_const_var.js
@@ -0,0 +1,7 @@
+let value=45;
+const value2=34;  // const value not updated
+var value3=54;
+value=23;
+// value2=21;  
+value3=12;
+console.log(value,value2,value3);
