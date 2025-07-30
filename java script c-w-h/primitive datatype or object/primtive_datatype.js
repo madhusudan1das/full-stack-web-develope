991fcbcd9eacbd28f1526eb4f4187e877576ddb4
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-07-30 10:03:52 +0530

    java script

---
+++ java script c-w-h/primitive datatype or object/primtive_datatype.js
@@ -0,0 +1,9 @@
+let a=null;
+let b=15;
+let c=true;
+let d=BigInt("32");
+let e="madhusudan";
+let f=Symbol("12@@@");
+let g=undefined;
+console.log(a,b,c,d,e,f,g);
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g);
