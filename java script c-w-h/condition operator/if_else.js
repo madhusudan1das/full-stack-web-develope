fa5b9a7b10ce1d003fa2d04636bc83c59e2a744b
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-07-31 11:58:50 +0530

    java script

---
+++ java script c-w-h/condition operator/if_else.js
@@ -0,0 +1,7 @@
+let value =prompt("enter the value ");
+value=Number.parseInt(value);
+if(value>=10){
+    console.log("value is higher: ");
+}else{
+console.log("value is low");
+}
---
+++ java script c-w-h/condition operator/ternary_if_else.js
@@ -0,0 +1,2 @@
+let age=54;
+console.log((age>=18)?"adult":"minor");
