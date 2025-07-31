1355e095f71e83a484cf503b5550a656f264b324
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-07-31 20:25:35 +0530

    java script

---
+++ java script c-w-h/condition operator/switch_case.js
@@ -0,0 +1,11 @@
+let value=prompt("enter the value");
+switch(true){
+    case (value>=90):
+        console.log("grade A");
+    break;
+    case (value>=60):
+    console.log("pass");
+    break;
+    default:
+        console.log("fail");
+}
